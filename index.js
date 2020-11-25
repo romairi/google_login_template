4a9dc1607bd1e4d8f0083122fbd3a4bc555a00ff
Roman Irinarhov <pyshok1991@gmail.com>
2020-11-25 18:59:51 +0200

    make it better

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-const PORT = 5000;
-
 app.get('/', (req, res) => {
     res.send({hi: "there"});
 });
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`app listening on the port ${PORT}`));
