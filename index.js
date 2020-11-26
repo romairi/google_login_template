276bbf73e47ab3eafdd64945537052f61b48f7e6
Roman Irinarhov <pyshok1991@gmail.com>
2020-11-26 15:16:23 +0200

    Server structure refactor

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./server/config/keys');
+require('./server/services/passport');
 
 
 const app = express();
 
-
-passport.use(new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, (accessToken) => {
-        console.log(accessToken);
-    })
-);
+require('./server/routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
