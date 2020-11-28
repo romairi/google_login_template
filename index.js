a55d932e383b7fb3f0ddaa641466fb37ebe7e561
Roman Irinarhov <pyshok1991@gmail.com>
2020-11-28 02:21:57 +0200

    Connect to MongoDB using mongoose

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./server/config/keys');
+require('./server/models/User');
 require('./server/services/passport');
 
+mongoose.connect(keys.mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    }, () => console.log('The connection succeeded to mongoDB!!!')
+);
 
 const app = express();
 
