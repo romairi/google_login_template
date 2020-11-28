bb684f8eed2fa7fb61b701625f639c275bd8bd8e
Roman Irinarhov <pyshok1991@gmail.com>
2020-11-28 17:12:30 +0200

    adding prod password and fix gitignore

--- index.js
+++ index.js
@@ -21,10 +21,3 @@ require('./server/routes/authRoutes')(app);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`app listening on the port ${PORT}`));
-
-
-//clientID = "759155158-1pe4vcds60g3nopqm6kmhht6alqd9t8n.apps.googleusercontent.com"
-//clientSecret = "pOfK8ZidxmnpbaV8Z0T-wOnU"
-// userName = admin_roman
-// password = xCX3a22N6fkRjHi6
-// connection = mongodb+srv://admin_roman:<password>@googleprod.87dyn.mongodb.net/<dbname>?retryWrites=true&w=majority
