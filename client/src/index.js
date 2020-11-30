1db26bca9acb7e59c1d4abd7d936c13ce85f136d
Roman Irinarhov <pyshok1991@gmail.com>
2020-11-30 10:55:07 +0200

    Add Provider to the app using Redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 
 import './index.css';
 import App from './components/App';
 
 
+const store = createStore(() => [], {}, applyMiddleware());
+
 ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
+    <Provider store={store}>
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    </Provider>,
     document.getElementById('root')
 );
