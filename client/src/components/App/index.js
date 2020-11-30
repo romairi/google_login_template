5d046f5795ac8f7a7def051edd5629ba878f6401
Roman Irinarhov <pyshok1991@gmail.com>
2020-11-30 08:35:22 +0200

    The architecture folder structure client side and install dependencies

---
+++ client/src/components/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './index.css';
+
+
+const App = () => {
+    return (
+        <div className="App">
+            Hi There!
+        </div>
+    );
+};
+
+export default App;
