3baf4d0a7ec680f6834d1bb26d19ebaa9f417e16
Roman Irinarhov <pyshok1991@gmail.com>
2020-12-02 18:13:37 +0200

    Implementation of fetchUserAction using redux thunk

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
+import {connect} from "react-redux";
 
+
+import * as actions from '../../redux/actions';
 import Routes from '../../routes/index';
 import Header from "../Header";
 
 
-const App = () => {
-    return (
-        <div>
-            <Router>
-                <Header/>
-                <Routes/>
-            </Router>
-        </div>
-    );
-};
+class App extends Component {
 
-export default App;
+    componentDidMount() {
+        console.log(this.props);
+        this.props.fetchUser();
+    }
+
+
+    render() {
+        return (
+            <div className="container">
+                <Router>
+                    <Header/>
+                    <Routes/>
+                </Router>
+            </div>
+        );
+    }
+}
+
+export default connect(null, actions)(App);
