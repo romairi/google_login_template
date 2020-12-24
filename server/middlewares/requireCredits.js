cda0bbb7f0538fa620882f5ec741c558a1e64b0d
Roman Irinarhov <pyshok1991@gmail.com>
2020-12-24 12:43:53 +0200

    Implementation sending email using "SendGrid"

--- server/middlewares/requireCredits.js
+++ server/middlewares/requireCredits.js
@@ -1,7 +1,7 @@
 const {StatusCodes} = require('http-status-codes');
 
 module.exports = (req, res, next) => {
-    if (req.user.credits < 1) {
+    if (req.user.credits < 0) {
         return res.status(StatusCodes.PAYMENT_REQUIRED).send({error: 'Not enough credits!'});
     }
 
--- server/models/Survey.js
+++ server/models/Survey.js
@@ -34,4 +34,4 @@ const surveySchema = new mongoose.Schema({
     },
 });
 
-module.exports = mongoose.model('Survey', surveySchema);
+module.exports = mongoose.model('surveys', surveySchema);
