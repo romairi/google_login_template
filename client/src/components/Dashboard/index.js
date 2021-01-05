8b4163fdd21352c15051615f6e789c303f5126c4
Roman Irinarhov <pyshok1991@gmail.com>
2021-01-05 06:08:33 +0200

    Implementation of SurveyList Component for Dashboard

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import {NEW_SURVEY} from "../../routes/constants";
 import './index.scss';
-
+import SurveyList from '../surveys/SurveyList';
 
 const Dashboard = () => {
     return (
         <div className='dashboard-container'>
-            Dashboard
+            <SurveyList/>
             <div className="fixed-action-btn">
                 <Link to={NEW_SURVEY} className="btn-floating btn-large waves-effect waves-light red">
                     <i className="material-icons">add</i>
